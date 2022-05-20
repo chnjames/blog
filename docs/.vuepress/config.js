e29431fad29416e5e479a69b796d608b64fa376a
一点一木 <44110547+chnjames@users.noreply.github.com>
2022-05-20 11:10:45 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,41 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
 module.exports = {
   base: '/blog',
   lang: 'zh-CN',
   title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点'
+  description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    navbar: [
+      // 嵌套 Group - 最大深度为 2
+      {
+        text: 'Group',
+        children: [
+          {
+            text: 'SubGroup',
+            children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+          },
+        ],
+      },
+      // 控制元素何时被激活
+      {
+        text: 'Group 2',
+        children: [
+          {
+            text: 'Always active',
+            link: '/',
+            // 该元素将一直处于激活状态
+            activeMatch: '/',
+          },
+          {
+            text: 'Active on /foo/',
+            link: '/not-foo/',
+            // 该元素在当前路由路径是 /foo/ 开头时激活
+            // 支持正则表达式
+            activeMatch: '^/foo/',
+          },
+        ],
+      },
+    ],
+  }),
 }
