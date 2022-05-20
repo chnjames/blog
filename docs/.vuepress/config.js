532d61e18899cf65e3fe2e45a6639d745de2b157
一点一木 <chnrural910@gmail.com>
2022-05-20 14:24:35 +0800

    Feat
    添加评论功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,16 @@ module.exports = {
   lang: 'zh-CN',
   title: '你好， VuePress !',
   description: '这是我的第一个 VuePress 站点',
+  plugins: [
+    ['@vssue/vuepress-plugin-vssue', {
+      platform: 'github',
+      owner: 'chnjames',
+      repo: 'blog',
+      clientId: 'fc6109bbcae36ff80c0c',
+      clientSecret: 'd06512d022ff2dea3f700b6002fd1273c56983d0',
+      autoCreateIssue: true //自动创建评论
+    }],
+  ],
   theme: defaultTheme({
     navbar: [
       // 嵌套 Group - 最大深度为 2
