69eaa7bbe1fa0277ec37d8a73e77deede3d373f6
一点一木 <chnrural910@gmail.com>
2022-06-16 11:08:17 +0800

    Feat: 更新文章github copilot

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -55,6 +55,7 @@ module.exports = {
                 '/software/tutorial.md',
                 '/software/platform.md'
             ]
-        }
+        },
+        '/software/GithubCopilot.md'
     ]
 }
