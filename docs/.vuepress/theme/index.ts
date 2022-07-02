deb0b9bfe357182a6ad4342c71e73949dcd76216
一点一木 <chnrural910@gmail.com>
2022-07-02 16:22:16 +0800

    Fix

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -4,11 +4,11 @@ import type { DefaultThemeOptions } from '@vuepress/theme-default'
 import { path } from '@vuepress/utils'
 
 export const localTheme = (options: DefaultThemeOptions): Theme => {
-    return {
-      name: 'vuepress-theme-local',
-      extends: defaultTheme(options),
-      layouts: {
-        Layout: path.resolve(__dirname, 'layouts/Layout.vue')
-      }
-    }
+  return {
+    name: 'vuepress-theme-local',
+    extends: defaultTheme(options),
+    // layouts: {
+    //   Layout: path.resolve(__dirname, 'layouts/Layout.vue')
+    // }
   }
+}
